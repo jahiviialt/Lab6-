6fd3ebe50b10befe73b382207baf315248b38cac
mositheboss1 <mr.doge2007@gmail.com>
2025-02-25 13:30:12 -0800

    Phone

--- Lab6/src/SmartPhone.java
+++ Lab6/src/SmartPhone.java
@@ -7,9 +7,29 @@ public class SmartPhone implements MusicPlayer, GPS, ScreenFeatures {
 	}
 
 	// TODO: methods from MusicPlayer interface
+	public void playSong(String songTitle) {
+		System.out.println("Now Playing: Smooth Criminal By Micheal Jackson");
+	}
+
+	public void downloadSong(String songTitle) {
+		System.out.println("Now Downloading Thriller By Micheal Jackson.");
+	}
 
 	// TODO: methods from GPS interface
+	public String getCurrentLocation() {
+		System.out.println("Getting Location...");
+		return "123 S Broad Street, Philadelphia.";
+	}
 
 	// TODO: methods from ScreenFeatures interface
+	public int getScreenSize() {
+		int length = 3;
+		int width = 4;
+		return (length * width);
+	}
 
+	public boolean isTouchScreen() {
+
+		return true;
+	}
 }
